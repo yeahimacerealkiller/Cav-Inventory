653067e26d50fecffb39a2526fc5df86a7946df1
kodumen <r.b.daitan@gmail.com>
2014-10-02 18:24:47 +0800

    Created table 'user' migration
    Table 'users' contains username, password, and full name of a user.

---
+++ app/database/migrations/2014_10_02_101739_create_table_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->string('username')->primary();
+            $table->string('password', 20); // 20 characters is long enough for a password, I think.
+            $table->string('first_name');
+            $table->string('last_name');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
