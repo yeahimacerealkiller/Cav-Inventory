9716600ae17720e02f9319261a3473ba39b92ba4
kodumen <r.b.daitan@gmail.com>
2014-10-02 18:43:56 +0800

    Created SessionController
    Created SessionController to handle login and logout.
    
    Routed main page to login form.

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,10 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+//Route::get('/', function()
+//{
+//	return View::make('hello');
+//});
+
+Route::resource('sessions', 'SessionsController');
+Route::get('/', 'SessionsController@create');   // Login Form
