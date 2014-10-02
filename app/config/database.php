4a5c9833f66a69d7955a1d8cbebda8310a2d973e
kodumen <r.b.daitan@gmail.com>
2014-10-02 17:23:27 +0800

    Set debug mode to true configured database driver

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'cav-inventory',
+			'username'  => 'root',
+			'password'  => 'restarted',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
