d9540d616fb26e964d12ea191e529858e6aa7429
kodumen <r.b.daitan@gmail.com>
2014-10-04 11:37:37 +0800

    Added the sidebar to the master page
    No hamburger yet.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -13,15 +13,29 @@ Template for ever page
         {{ HTML::style('css/pure-release-0.5.0/pure-min.css'); }}
         <!--[if lte IE 8]>
             {{ HTML::style('css/pure-release-0.5.0/grids-responsive-old-ie-min.css'); }}
+            {{ HTML::style('css/layouts/side-menu-old-ie.cs') }}
         <![endif]-->
         <!--[if gt IE 8]><!-->
             {{ HTML::style('css/pure-release-0.5.0/grids-responsive-min.css'); }}
+            {{ HTML::style('css/layouts/side-menu.css') }}
         <!--<![endif]-->
-        
+        {{ HTML::script('js/ui.js') }}
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <!--NAV SIDEBAR-->
-        
+        <div clas="layout">
+            <!--The menu itself-->
+            <div id="menu">
+                <div class="pure-menu pure-menu-open">
+                    <a class="pure-menu-heading" href="{{ action('UserProfileController@show', Auth::user()->username) }}">
+                        {{ Auth::user()->username }}
+                    </a>
+                </div>
+            </div>
+            <div id="main">
+            
+            </div>
+        </div>
     </body>
 </html>
